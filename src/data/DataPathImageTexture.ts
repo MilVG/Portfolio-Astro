587a7b5235701a6102ee799536f2b11628efda33
notlim_dev <elmotigre6@gmail.com>
2025-04-21 00:17:53 -0500

    Array de Objetos de Imagenes Para la eccion de Skills

---
+++ src/data/DataPathImageTexture.ts
@@ -0,0 +1,162 @@
+export type dataPathTextureT = {
+  id: number
+  image: string
+  positionMesh: [number, number, number]
+  positionDecal: [number, number, number]
+  rotation: [number, number, number]
+  scalemesh: number
+  scaleDecal: number
+}
+export const dataPathTexture: dataPathTextureT[] = [
+  {
+    id: 1,
+    image: "js",
+    positionMesh: [0.7, 2.9, 0],
+    positionDecal: [0, 0, 1],
+    rotation: [0.5, 0, 0],
+    scalemesh: 0.3,
+    scaleDecal: 0.4
+  },
+  {
+    id: 2,
+    image: "python",
+    positionMesh: [2.3, 1.84, 0],
+    positionDecal: [-0.3, 0, 1],
+    rotation: [0, 0, 0],
+    scalemesh: 0.3,
+    scaleDecal: 0.4
+
+  },
+  {
+    id: 3,
+    image: "php",
+    positionMesh: [-2.7, -1.4, 0],
+    positionDecal: [0, 0, 1],
+    rotation: [0, 0.4, 0],
+    scalemesh: 0.3,
+    scaleDecal: 0.4
+  },
+  {
+    id: 4,
+    image: "java",
+    positionMesh: [-1.22, 2.8, 0],
+    positionDecal: [0.3, 0, 1],
+    rotation: [0, 0, 0],
+    scalemesh: 0.3,
+    scaleDecal: 0.6
+
+  },
+  {
+    id: 5,
+    image: "cplus",
+    positionMesh: [2.3, -1.8, 0],
+    positionDecal: [0, 0, 1],
+    rotation: [0, -0.3, 0],
+    scalemesh: 0.3,
+    scaleDecal: 0.4
+  },
+  {
+    id: 6,
+    image: "ts",
+    positionMesh: [-2.8, 1.5, 0],
+    positionDecal: [0, 0, 1],
+    rotation: [0.4, 0.2, 0],
+    scalemesh: 0.3,
+    scaleDecal: 0.4
+
+  },
+  {
+    id: 7,
+    image: "react",
+    positionMesh: [0, 0, 0],
+    positionDecal: [0, 0, 1],
+    rotation: [0, 0, 0],
+    scalemesh: 0.6,
+    scaleDecal: 0.4
+
+  },
+  {
+    id: 8,
+    image: "taildwindCss",
+    positionMesh: [0.1, 1.4, 0],
+    positionDecal: [0, 0, 1],
+    rotation: [0, 0, 0],
+    scalemesh: 0.3,
+    scaleDecal: 0.4
+
+  },
+  {
+    id: 9,
+    image: "nextjs",
+    positionMesh: [3, 0, 0],
+    positionDecal: [0, 0, 1],
+    rotation: [0, 0, 0],
+    scalemesh: 0.3,
+    scaleDecal: 0.4
+
+  },
+  {
+    id: 10,
+    image: "laravel",
+    positionMesh: [-1.2, 0.9, 0],
+    positionDecal: [0, 0, 1],
+    rotation: [0, 0, 0],
+    scalemesh: 0.3,
+    scaleDecal: 0.4
+
+  },
+  {
+    id: 11,
+    image: "postgreSql",
+    positionMesh: [0.7, -2.8, 0],
+    positionDecal: [0, 0, 1],
+    rotation: [-0.4, 0, 0],
+    scalemesh: 0.3,
+    scaleDecal: 0.4
+  },
+  {
+    id: 12,
+    image: "mongoDB",
+    positionMesh: [-1.2, -2.7, 0],
+    positionDecal: [0, 0, 1],
+    rotation: [-0.6, 0.1, 0],
+    scalemesh: 0.3,
+    scaleDecal: 0.4
+  },
+  {
+    id: 13,
+    image: "gitHub",
+    positionMesh: [1.25, 0.7, 0],
+    positionDecal: [0, 0, 1],
+    rotation: [0, 0, 0],
+    scalemesh: 0.3,
+    scaleDecal: 0.4
+  },
+  {
+    id: 14,
+    image: "docker",
+    positionMesh: [-1.16, -0.87, 0],
+    positionDecal: [0, 0, 1],
+    rotation: [-0.2, 0.2, 0],
+    scalemesh: 0.3,
+    scaleDecal: 0.4
+  },
+  {
+    id: 15,
+    image: "unity",
+    positionMesh: [0.14, -1.48, 0],
+    positionDecal: [0, 0, 1],
+    rotation: [0, 0, 0],
+    scalemesh: 0.3,
+    scaleDecal: 0.4
+  },
+  {
+    id: 16,
+    image: "AR-VR",
+    positionMesh: [1.28, -0.75, 0],
+    positionDecal: [0, 0, 1],
+    rotation: [-0.4, 0, 0],
+    scalemesh: 0.3,
+    scaleDecal: 0.4
+  },
+]
