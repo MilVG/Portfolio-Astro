f0647a16ead58ce3ff13228636e66427f4cea980
notlim_dev <elmotigre6@gmail.com>
2025-04-21 00:19:07 -0500

    Array de Texros para los Labels de la seccion de About

---
+++ src/data/labeldata.ts
@@ -0,0 +1,33 @@
+export type LabelsDataTypes = {
+  id: string
+  title: string
+  description: string
+}
+
+export const labelsData: LabelsDataTypes[] = [
+  {
+    id: "label_01",
+    title: "NotlimDev",
+    description: "El Verdadero Trabajo de un profesional es asegurar la calidad de Software",
+  },
+  {
+    id: "label_02",
+    title: "Evolución",
+    description: "Me Apasiona estar actualizado con las Nuevas tecnologías",
+  },
+  {
+    id: "label_03",
+    title: "Creatividad",
+    description: "El punto clave en la tecnología es aportar nuevas y valisosas ideas.",
+  },
+  {
+    id: "label_04",
+    title: "Equipo",
+    description: "La riqueza de cada Persona, crece al escuchar y ser participe de su grupo de Trabajo.",
+  },
+  {
+    id: "label_05",
+    title: "Relajación",
+    description: "Despejar tu mente es saludable, Escoje lo que más te guste para Distraerte",
+  }
+]
